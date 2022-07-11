b9071e989fb6e487dd10aee3354b059ed7933523
FieBee <phuonglf98@gmail.com>
2022-07-11 10:53:29 +0700

    add TH-Animal and interface Edible

--- TH - Animal and interface Edible/src/Chicken.java
+++ TH - Animal and interface Edible/src/Chicken.java
@@ -1,6 +1,11 @@
-public class Chicken extends Animal{
+public class Chicken extends Animal implements Edible{
     @Override
     public String makeSound(){
         return "Chicken: quac quac quac";
+    }
+
+    @Override
+    public String howToEat() {
+        return "Gaugaugau";
     }
 }
